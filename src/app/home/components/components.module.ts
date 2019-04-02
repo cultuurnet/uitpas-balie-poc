95c998a7f319f283038fb9b69ab6afb5f6378155
Stijn Swaanen <swaanenstijn@gmail.com>
2019-04-02 14:32:17 +0200

    UPS-2797 homepage

--- src/app/home/components/components.module.ts
+++ src/app/home/components/components.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from '@angular/core';
 import {LocationHeaderComponent} from './location-header/location-header.component';
 import {IonicModule} from '@ionic/angular';
 import {CommonModule} from '@angular/common';
+import {SmartphoneComponent} from './smartphone/smartphone.component';
+import {NfcComponent} from './nfc/nfc.component';
 
 @NgModule({
     imports: [
@@ -9,10 +11,14 @@ import {CommonModule} from '@angular/common';
         CommonModule
     ],
     declarations: [
-        LocationHeaderComponent
+        LocationHeaderComponent,
+        SmartphoneComponent,
+        NfcComponent
     ],
     exports: [
-        LocationHeaderComponent
+        LocationHeaderComponent,
+        SmartphoneComponent,
+        NfcComponent
     ]
 })
 
---
+++ src/app/home/components/smartphone/smartphone.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-smartphone',
+  templateUrl: './smartphone.component.html',
+  styleUrls: ['./smartphone.component.scss'],
+})
+export class SmartphoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
